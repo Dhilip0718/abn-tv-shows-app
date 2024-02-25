8910316f97d37a39e175fcd125e3daf45e95f9f0
Dhilip0718 <dhilipram@gmail.com>
2024-02-25 21:17:21 +0100

    updates on router configs and search by id included.

---
+++ abn-tv-show-app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MovieList from '@/components/MovieList.vue';
+import SearchBar from '@/components/SearchBar.vue';
+</script>
+
+<template>
+  <main>
+    <SearchBar />
+    <MovieList />
+  </main>
+</template>
